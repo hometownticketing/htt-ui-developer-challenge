9717190101381040c96a9828790fa61a1a4a2ca3
Hassan <aaron@yobo.dev>
2023-06-16 18:28:47 -0400

    Completed the challenge

--- src/app/challenge/page.tsx
+++ src/app/challenge/page.tsx
@@ -1,5 +1,14 @@
+import Card from "../components/card";
+interface IUser {
+  id: string;
+  name: string;
+  role: string;
+  bio: string;
+  image: string;
+  description: string;
+}
 async function getData() {
-  const res = await fetch('http://localhost:3000/api');
+  const res = await fetch("http://localhost:3000/api");
 
   if (!res.ok) {
     throw new Error(res.statusText);
@@ -14,8 +23,19 @@ export default async function Challenge() {
   return (
     <main className="flex text-black font-serif min-h-screen flex-col  items-start justify-start p-4 w-full">
       <h1 className="text-4xl font-bold text-center">The Challenge</h1>
-      <div className="flex flex-row flex-wrap gap-4">
-      </div> 
+      <div className="flex flex-row flex-wrap justify-start gap-4">
+        {data.map((user: IUser) => (
+          <Card
+            name={user.name}
+            id={user.id}
+            title={user.role}
+            subTitle={user.bio}
+            bodyText={user.description}
+            linkText={user.image}
+            className="basis-1/2"
+          />
+        ))}
+      </div>
     </main>
-  )
+  );
 }
--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,5 +1,5 @@
-import React, { PropsWithChildren } from 'react';
-import classNames from 'classnames';
+import React, { PropsWithChildren } from "react";
+import classNames from "classnames";
 
 export interface CardInterface extends PropsWithChildren {
   className?: string;
@@ -16,30 +16,72 @@ export interface CardInterface extends PropsWithChildren {
   dataTest?: string;
 }
 
-export default function Card({ className, id = 0, name, title, subTitle, bodyText, children, count = 0, quantity = 0, link = '#', linkText, onClick, dataTest }: CardInterface) {
-  const cardId = `${ id }`;
+export default function Card({
+  className,
+  id = 0,
+  name,
+  title,
+  subTitle,
+  bodyText,
+  children,
+  count = 0,
+  quantity = 0,
+  link = "#",
+  linkText,
+  onClick,
+  dataTest,
+}: CardInterface) {
+  const cardId = `${id}`;
 
   return (
-    <div className={classNames("flex flex-wrap border-2 border-gray-dark rounded-md mt-6 text-gray-dark dark:b-gray-dark dark:border-gray-cool dark:text-white", className)} id={cardId} data-test="card">
-      <div className="content flex flex-wrap p-4 w-full">
-        { title && (
-          <h1 className="headline text-xl font-bold capitalize w-full">{title}</h1>
-        )}
-        { subTitle && (
-          <h2 className="w-full text-base font-semibold capitalize">{subTitle}</h2>
-        )}
-        {bodyText}
-        {children}
+    <div
+      className={classNames(
+        "flex flex-wrap max-w-xl border-2 p-6 border-gray-dark rounded-md mt-6 text-gray-dark dark:b-gray-dark dark:border-gray-cool dark:text-white hover:border-red-400",
+        className
+      )}
+      id={cardId}
+      data-test="card"
+    >
+      {name && (
+        <h1 className="headline text-xl font-bold capitalize w-full text-gray-600 p-1">
+          {name}
+        </h1>
+      )}
+      <div className="flex flex-row">
+        <img src={linkText} className="max-h-24 max-w-24 p-4" />
+        <div className="content flex flex-wrap p-4 w-full text-gray-800">
+          {title && (
+            <h1 className="headline text-xl font-bold capitalize w-full text-blue-400">
+              {title}
+            </h1>
+          )}
+          {subTitle && (
+            <h2 className="w-full text-base font-semibold capitalize text-gray-400">
+              {subTitle}
+            </h2>
+          )}
+          {bodyText}
+          {children}
+        </div>
       </div>
-      <div className="flex justify-end items-center w-full pl-4 pr-4 pt-2 pb-2 bg-gray-light rounded-bl-md rounded-br-md text-sm" data-test="card-footer">
+
+      <div
+        className="flex justify-end text-gray-800 items-center w-full pl-4 pr-4 pt-2 pb-2 bg-gray-light rounded-bl-md rounded-br-md text-sm"
+        data-test="card-footer"
+      >
         <a
           href={link}
-          className="flex flex-row font-normal capitalize border-b-2 border-transparent hover:border-black cursor-pointer pb-0 pr-5 relative leading-none"
+          className="flex  flex-row font-normal capitalize border-b-2 border-transparent hover:border-black cursor-pointer pb-0 pr-5 relative leading-none"
           data-test={dataTest}
-          {...(link === '#' && onClick ? { onClick } : { onClick: () => {} })}
+          // {...(link === '#' && onClick ? { onClick } : { onClick: () => {} })}
         >
-          {linkText}
-          <span className="absolute top-0.5 right-0 leading-none" data-test="card-footer-option">&#8594;</span>
+          See Profile
+          <span
+            className="absolute top-0.5 right-0 leading-none"
+            data-test="card-footer-option"
+          >
+            &#8594;
+          </span>
         </a>
       </div>
     </div>
