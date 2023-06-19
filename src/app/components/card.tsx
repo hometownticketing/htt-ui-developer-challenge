0dfa9826a59944b81115ef94421521a6d734371a
Bryan Cunningham <bryan.cunningham@me.com>
2023-06-19 13:03:57 -0400

    Update Card UI to match mock

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,4 +1,5 @@
-import React, { PropsWithChildren } from 'react';
+'use client'
+import React, { MouseEvent, PropsWithChildren } from 'react';
 import classNames from 'classnames';
 
 export interface CardInterface extends PropsWithChildren {
@@ -12,36 +13,56 @@ export interface CardInterface extends PropsWithChildren {
   quantity?: number;
   link?: string;
   linkText: string;
-  onClick?: (e: MouseEvent | KeyboardEvent) => void;
+  onClick?: (e: MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
   dataTest?: string;
+  isActive: boolean;
 }
 
-export default function Card({ className, id = 0, name, title, subTitle, bodyText, children, count = 0, quantity = 0, link = '#', linkText, onClick, dataTest }: CardInterface) {
+export const Card = ({ className, id = 0, name, title, subTitle, bodyText, children, count = 0, quantity = 0, link = '#', linkText, onClick, dataTest, isActive }: CardInterface) => {
   const cardId = `${ id }`;
 
   return (
-    <div className={classNames("flex flex-wrap border-2 border-gray-dark rounded-md mt-6 text-gray-dark dark:b-gray-dark dark:border-gray-cool dark:text-white", className)} id={cardId} data-test="card">
-      <div className="content flex flex-wrap p-4 w-full">
-        { title && (
-          <h1 className="headline text-xl font-bold capitalize w-full">{title}</h1>
-        )}
-        { subTitle && (
-          <h2 className="w-full text-base font-semibold capitalize">{subTitle}</h2>
-        )}
-        {bodyText}
-        {children}
-      </div>
-      <div className="flex justify-end items-center w-full pl-4 pr-4 pt-2 pb-2 bg-gray-light rounded-bl-md rounded-br-md text-sm" data-test="card-footer">
-        <a
-          href={link}
-          className="flex flex-row font-normal capitalize border-b-2 border-transparent hover:border-black cursor-pointer pb-0 pr-5 relative leading-none"
-          data-test={dataTest}
-          {...(link === '#' && onClick ? { onClick } : { onClick: () => {} })}
-        >
-          {linkText}
-          <span className="absolute top-0.5 right-0 leading-none" data-test="card-footer-option">&#8594;</span>
-        </a>
+    <div className={classNames(`border-2 flex flex-col ${isActive ? 'border-red-500' : 'border-gray-dark'} rounded-md text-gray-dark p-2 transition-colors`, className)} id={cardId} data-test="card">
+      {name && (
+        <p className='font-bold mb-2'>{name}</p>
+      )}
+      <div className='flex flex-col flex-grow justify-between'>
+        <div className='flex justify-between'>
+          <div className='w-24 min-w-24'>
+            {children}
+          </div>
+          <div className='pl-4'>
+            {title && (
+              <h2 className="headline text-l font-bold text-blue-600 capitalize w-full">{title}</h2>
+            )}
+            {subTitle && (
+              <p className="w-full text-base text-gray-400 capitalize">{subTitle}</p>
+            )}
+            <p className="w-full text-base text-gray-500">
+              {bodyText}
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-end items-center p-4 pb-0 text-sm" data-test="card-footer">
+          <a
+            href={link}
+            className="flex flex-row font-normal align-center capitalize border-b-2 border-transparent hover:border-black cursor-pointer pb-0 pr-5 relative leading-none"
+            data-test={dataTest}
+            onClick={(e) => {
+              // Just preventing default here to keep the page from scrolling back to the top
+              // since we are not actually linking to the profile
+              // This is not a real life scenario. Would never do this in a prod env
+              e.preventDefault();
+              onClick?.(e)
+            }}
+          >
+            {linkText}
+            <span className="absolute top-0.5 right-0 leading-none" data-test="card-footer-option">&#8594;</span>
+          </a>
+        </div>
       </div>
     </div>
   );
 }
+
+export default Card
